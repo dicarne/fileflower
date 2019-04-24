1507185d74f17bd627b9be901095f24e10c79d1c
dicarne <hzdingjiahe@outlook.com>
2019-04-24 22:19:45 +0800

    初步实现可以展开合起的图

--- FilesFlower.js
+++ FilesFlower.js
@@ -1,47 +1,213 @@
 var fs = require("fs")
 let join = require('path').join;
+let d3 = require("d3")
 
-FilesFlower = function () {
-    console.log("hello world")
+FilesFlower = function (selector, w, h) {
+    this.w = w;
+    this.h = h;
+    this.selector=selector;
+    d3.select(selector).selectAll("svg").remove();
+
+    this.svg = d3.select(selector).append("svg:svg")
+        .attr('width', w)
+        .attr('height', h);
+
+    this.svg.append("svg:rect")
+        .style("stroke", "#999")
+        .style("fill", "#fff")
+        .attr('width', w)
+        .attr('height', h);
+    this.filestree = {};
+    this.node = {};
+    this.link = {};
 }
 
 module.exports = FilesFlower
 
-FilesFlower.prototype.ScanFileSystem = function () {
-
-    /**
-     * 
-     * @param startPath  起始目录文件夹路径
-     * @returns {Array}
-     */
-    function findSync(startPath) {
-        let result = [];
-        function finder(path) {
-            let files = fs.readdirSync(path);
-            files.forEach((val, index) => {
-                let fPath = join(path, val);
-                let stats = fs.statSync(fPath);
-                let file = {}
-                try {
-                    if (stats.isDirectory()) finder(fPath);
-                    if (stats.isFile()) {
-                        file.path = fPath
-                        file.size = stats.size
-                        result.push(file);
-                    }
-                } catch{
-                    file.path = fPath
-                    file.size = stats.size
-                    result.push(file)
-                }
-            });
-
-        }
-        finder(startPath);
-        return result;
+FilesFlower.prototype.update = function (rootPath) {
+    d3.select(this.selector).selectAll("g").remove();
+    if (rootPath)
+        this.filestree = this.ScanFileSystem(rootPath);
+    console.log(this.filestree);
+    const nodes = this.flatten(this.filestree);
+    console.log(this.links)
+    //const total = nodes.length || 1;
+    if (nodes.length === 1) {
+        this.links = [];
     }
 
-    let fileNames = findSync('C://bash');
-    console.log(fileNames)
+    this.svg.selectAll("text").remove();
+    this.simulation = d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(this.links).id(d => d.name))
+        .force("charge", d3.forceManyBody().strength(-100))
+        .force("center", d3.forceCenter(this.w / 2, this.h / 2));
+
+    this.link = this.svg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(this.links)
+        .join("line")
+    //.attr("stroke-width", d => Math.sqrt(d.value));
+
+    this.node = this.svg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", 10)
+        .attr("fill", color)
+        .call(this.drag(this.simulation))
+        .on("click", this.click.bind(this));
+
+    this.simulation.on("tick", () => {
+        this.link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+        this.node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y)
+    });
+
+}
+color = d => {
+    if(d.type == "dir"){
+        return "#66ccff";
+    }
+    if(d.type == "file"){
+        return "#223344"
+    }
+}
+FilesFlower.prototype.drag = function (simulation) {
+
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
+
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
 }
 
+FilesFlower.prototype.click = function (d) {
+
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+    } else {
+        d.children = d._children;
+        d._children = null;
+    }
+    this.update();
+};
+
+FilesFlower.prototype.finder = function (path, parent) {
+    let files = fs.readdirSync(path);
+    var totalsize = 0;
+    files.forEach((val, index) => {
+        let fPath = join(path, val);
+        let stats = fs.statSync(fPath);
+        // 当前文件
+        let file = {
+            name: val,
+            path: fPath,
+            size: stats.size,
+            children: null,
+            _children: [],
+            error: false,
+            parent: parent
+        };
+        // 加入父节点
+        parent._children.push(file);
+        try {
+            if (stats.isDirectory()) {
+                file.type = "dir";
+                let subfile = this.finder(fPath, file);
+                file.size += subfile;
+                totalsize += file.size;
+            }
+            if (stats.isFile()) {
+                file.type = "file";
+                totalsize += file.size;
+            }
+        } catch{
+            file.error = true;
+        }
+    });
+
+    return totalsize;
+}
+
+/**
+   * 
+   * @param startPath  起始目录文件夹路径
+   * @returns {Array}
+   */
+FilesFlower.prototype.findSync = function (startPath) {
+    let result = [];
+    // 根节点
+    let root = { name: "root", path: startPath, children: null, _children: [], parent: null , type:"dir"}
+    this.links = [];
+
+    this.finder(startPath, root);
+    return root;
+}
+
+/**
+ * 根据根目录扫描文件系统
+ */
+FilesFlower.prototype.ScanFileSystem = function (path) {
+
+    let fileNames = this.findSync(path);
+    return fileNames;
+}
+
+FilesFlower.prototype.try = function () {
+    this.ScanFileSystem('C://bash');
+}
+
+
+// 展开树
+FilesFlower.prototype.flatten = function (root) {
+    var nodes = [];
+    var i = 0;
+    this.links = [];
+    function recurse(node, nodes, links) {
+
+        if (node.children) {
+            node.size = node.children.reduce(function (p, v) {
+                return p + recurse(v, nodes, links);
+            }, 0);
+        }
+
+        if (!node.id) node.id = ++i;
+        if (node.parent != null) {
+            links.push({
+                source: node.parent.name,
+                target: node.name
+            });
+        }
+        nodes.push(node);
+        return node.size;
+    }
+
+    root.size = recurse(root, nodes, this.links);
+    return nodes;
+}
